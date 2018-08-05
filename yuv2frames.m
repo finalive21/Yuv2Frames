1734f344126f9b1715068a4e199d31b6e5b38015
finalive21 <42043422+finalive21@users.noreply.github.com>
2018-08-05 01:29:33 -0500

    Add files via upload

--- yuv2frames.m
+++ yuv2frames.m
@@ -1,45 +1,42 @@
+%% 
+clc;
 clear all;
-clc all;
 close all;
 
-%% Extraer frames de un formato de video YUV 4:2:0
+%05/08/18
 
-clc; clear; close all;
+%creo una estructura llamada frames con 3 campos, teniendo anidada otra estructura en el
+%campo 3 correspondiente a las dimensiones
+field1 = 'name';
+field2 = 'frame';
+field3 = 'dim';
 
-%Los videos se encuentran en formato YUV 4:2:0
+%creo una estructura anidada vacia con campos ya definidos
+field3_1 = 'width';
+field3_2 = 'height';
+value3 = struct(field3_1,{},field3_2,{});
 
-%Se introduce la informacion del video
+%creo una estructura vacia con campos ya definidos
+frames = struct(field1,{},field2,{},field3,value3);
+
+%extraigo frames de la componente Luma (Y) del video con formato YUV 4:2:0
+
 videoSequence = 'akiyo_qcif.yuv';
 width  = 176;
 height = 144;
 nFrame = 300;
 
-
 % Lectura de la secuencia de video
 [Y,U,V] = yuvRead(videoSequence, width, height ,nFrame); 
 
+%en Y se encuentran una cantidad de cuadros 'nFrame' en escala de grises
 
-% Se escogen los frames deseados
-%Los valores son tomados como ejemplo
-
-frame_first = 5;
-frame_last = 300;
-
-
-%Como nos interesa el componente relacionado con la luminancia, se
-%trabajara sobre la variable Y
-
-
-figure;
-j=1;
-for i=frame_first:5:frame_last
-    subplot(6,10,j), imshow(Y(:,:,i)), title(['frame' num2str(i)]);
-    j=j+1;
-    
+%a cada elemento de la estructura le asignare datos en los campos
+%correspondientes
+for i=1:nFrame
+    frames(i).name = ['Frame ' num2str(i)];
+    frames(i).frame = Y(:,:,i);
+    frames(i).dim.width = width;
+    frames(i).dim.height = height;
 end
-
-%Guardo un frame de la capa de luminancia
-imwrite(Y(:,:,70),'my_frame.png');
-
-%Cantidad de frames a mostrar en el subplot= (final-inicial)/razon + 1
 
